7a0ce4a3320bb2d7b94a5b548d09f00025aac50e
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-07-13 10:22:36 +0200

    black formatting

--- models.py
+++ models.py
@@ -1,16 +1,15 @@
-
 class Client:
     def __init__(
-            self,
-            client_id=None,
-            first_name=None,
-            last_name=None,
-            email=None,
-            telephone=None,
-            company_name=None,
-            created=None,
-            modified=None,
-            commercial_id=None
+        self,
+        client_id=None,
+        first_name=None,
+        last_name=None,
+        email=None,
+        telephone=None,
+        company_name=None,
+        created=None,
+        modified=None,
+        commercial_id=None,
     ):
         client_id = client_id
         first_name = first_name
@@ -25,14 +24,13 @@ class Client:
 
 class Contract:
     def __init__(
-            self,
-            contract_id=None,
-            client_id=None,
-            total_amount=None,
-            due_amount=None,
-            created=None,
-            status=None,
-
+        self,
+        contract_id=None,
+        client_id=None,
+        total_amount=None,
+        due_amount=None,
+        created=None,
+        status=None,
     ):
         contract_id = contract_id
         client_id = client_id
@@ -44,16 +42,16 @@ class Contract:
 
 class Event:
     def __init__(
-            self,
-            event_id=None,
-            contract_id=None,
-            client_id=None,
-            start_date=None,
-            end_date=None,
-            support_contact_id=None,
-            location=None,
-            attendees=None,
-            notes=None
+        self,
+        event_id=None,
+        contract_id=None,
+        client_id=None,
+        start_date=None,
+        end_date=None,
+        support_contact_id=None,
+        location=None,
+        attendees=None,
+        notes=None,
     ):
         event_id = event_id
         contract_id = contract_id
@@ -68,17 +66,15 @@ class Event:
 
 class Employee:
     def __init__(
-            self,
-            employee_id=None,
-            first_name=None,
-            last_name=None,
-            email=None,
-            department_id=None
+        self,
+        employee_id=None,
+        first_name=None,
+        last_name=None,
+        email=None,
+        department_id=None,
     ):
         employee_id = employee_id
         first_name = first_name
         last_name = last_name
         email = email
         department_id = department_id
-
-
