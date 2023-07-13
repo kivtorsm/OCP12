a8fdf81bf016b2238a09c81c678a50c68e64f75e
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-07-13 10:16:30 +0200

    initial dao classes

---
+++ dao.py
@@ -0,0 +1,42 @@
+
+class ClientDAO:
+    def client_get(self):
+        pass
+    
+    def client_save(self):
+        pass
+    
+    def client_update(self):
+        pass
+    
+    def client_delete(self):
+        pass
+
+
+class ContractDAO:
+    def contract_get(self):
+        pass
+
+    def contract_save(self):
+        pass
+
+    def contract_update(self):
+        pass
+
+    def contract_delete(self):
+        pass
+
+    class EventDAO:
+        def event_get(self):
+            pass
+
+        def event_save(self):
+            pass
+
+        def event_update(self):
+            pass
+
+        def event_delete(self):
+            pass
+        
+        
