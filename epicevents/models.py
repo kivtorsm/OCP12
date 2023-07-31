6d54fab018b6d6fec14139dc34e097cc0f2b1cd5
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-07-31 10:17:53 +0200

    added relationships

--- epicevents/models.py
+++ epicevents/models.py
@@ -1,65 +1,26 @@
 from database import db
 
-from sqlalchemy import Column, Integer, String, Float, create_engine
+from sqlalchemy import Column, Integer, String, Float, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 
 Base = declarative_base()
 
 
-class Client(Base):
-    __tablename__ = "clients"
-
-    # Data columns
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String(30))
-    last_name = Column(String(30))
-    email = Column(String(100))
-    telephone = Column(String(15))
-    company_name = Column(String(50))
-    commercial_id = Column(Integer)
-
-    # Audit columns
-    created = Column(db.DATETIME)
-    modified = Column(db.DATETIME)
-
-    def __repr__(self):
-        return "<Client(first_name='%s', last_name='%s', company_name='%s')>" % (
-            self.first_name,
-            self.last_name,
-            self.company_name,
-        )
-
-
-class Contract(Base):
-    __tablename__ = "contracts"
-
-    # Data columns
-    id = Column(Integer, primary_key=True)
-    client_id = Column(Integer)
-    total_amount = Column(Float)
-    due_amount = Column(Float)
-    status = Column(String(20))
-
-    # Audit columns
-    created = Column(db.DATETIME)
-
-    def __repr__(self):
-        return f"Contract {self.id}, client {self.client_id}, total_amount {self.total_amount}, " \
-               f"due_amount {self.due_amount}"
-
-
 class Event:
-    __tablename__ = "events"
+    __tablename__ = "event"
 
     # Data columns
     id = Column(Integer, primary_key=True)
-    contract_id = Column(Integer)
-    client_id = Column(Integer)
+    contract_id = Column(Integer, ForeignKey('contract.id'))
+    contract = relationship('Contract')
+    client_id = Column(Integer, ForeignKey('client.id'))
+    client = relationship('Client')
     start_date = Column(db.DATETIME)
     end_date = Column(db.DATETIME)
-    support_contact_id = Column(Integer)
+    support_contact_id = Column(Integer, ForeignKey('employee.id'))
+    support_contact = relationship('Employee')
     location = Column(String(100))
     attendees_number = Column(Integer)
     notes = Column(String(2048))
@@ -74,55 +35,109 @@ class Event:
                f"attendees {self.attendees_number}"
 
 
+class Contract(Base):
+    __tablename__ = "contract"
+
+    # Data columns
+    id = Column(Integer, primary_key=True)
+    client_id = Column(Integer, ForeignKey('client.id'))
+    client = relationship('Client')
+    total_amount = Column(Float)
+    due_amount = Column(Float)
+    status = Column(String(20))
+    events = relationship(Event, backref="events")
+
+    # Audit columns
+    created = Column(db.DATETIME)
+
+    def __repr__(self):
+        return f"Contract {self.id}, client {self.client.company_name}, total_amount {self.total_amount}, " \
+               f"due_amount {self.due_amount}"
+
+
+class Client(Base):
+    __tablename__ = "client"
+
+    # Data columns
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    email = Column(String(100))
+    telephone = Column(String(15))
+    company_name = Column(String(50))
+    commercial_id = Column(Integer, ForeignKey('employee.id'))
+    contracts = relationship(Contract, backref='contracts')
+    events = relationship(Event, backref='clients')
+
+    # Audit columns
+    created = Column(db.DATETIME)
+    modified = Column(db.DATETIME)
+
+    def __repr__(self):
+        return "<Client(first_name='%s', last_name='%s', company_name='%s')>" % (
+            self.first_name,
+            self.last_name,
+            self.company_name,
+        )
+
+
 class Employee:
-    __tablename__ = "employees"
+    __tablename__ = "employee"
 
     # Data columns
     id = Column(Integer, primary_key=True)
     first_name = Column(String(20))
     last_name = Column(String(20))
     email = Column(String(100))
-    department_id = Column(Integer)
+    department_id = Column(Integer, ForeignKey='department.id')
+    department = relationship('Department')
     encoded_hash = Column(String(64))
+    commercial_for = relationship(Client, backref='clients')
+    support_contact_for = relationship(Event, backref='events')
 
     def __repr__(self):
         return f"Employee {self.id}, first name {self.first_name}, last name {self.last_name}, " \
                f"email {self.email}, department id {self.department_id}"
 
 
+class DepartmentPermission:
+    __tablename__ = "department_permission"
+
+    # Data columns
+    id = Column(Integer, primary_key=True)
+    department_id = Column(Integer, ForeignKey='department.id')
+    department = relationship('Department')
+    permission_id = Column(Integer, ForeignKey='permission.id')
+    permission = relationship('Permission')
+
+    def __repr__(self):
+        return f"Department permission {self.id}, department {self.deparment.name}, " \
+               f"permission {self.permission.name}"
+
+
 class Department:
-    __tablename__ = "departments"
+    __tablename__ = "department"
 
     # Data columns
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
+    employees = relationship(Employee, backref='employees')
+    department_permission = relationship(DepartmentPermission, backref='departmentpermissions')
 
     def __repr__(self):
         return f"Department {self.id}, name {self.name}"
 
 
 class Permission:
-    __tablename__ = "permissions"
+    __tablename__ = "permission"
 
     # Data columns
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
+    department_permission = relationship(DepartmentPermission, backref='departmentpermissions')
 
     def __repr__(self):
         return f"Permission {self.id}, name {self.name}"
-
-
-class DepartmentPermission:
-    __tablename__ = "department_permissions"
-
-    # Data columns
-    id = Column(Integer, primary_key=True)
-    department_id = Column(Integer)
-    permission_id = Column(Integer)
-
-    def __repr__(self):
-        return f"Department permission {self.id}, department id {self.department_id}, " \
-               f"permission id {self.permission_id}"
 
 
 def start():
